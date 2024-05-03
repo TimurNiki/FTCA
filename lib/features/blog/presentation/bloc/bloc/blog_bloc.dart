ca41b50d00ca254dc160051be08a8493599108e9
TimurNiki <timurniki95@gmail.com>
2024-05-03 21:30:16 +0400

    bloc

--- lib/features/blog/presentation/bloc/bloc/blog_bloc.dart
+++ lib/features/blog/presentation/bloc/bloc/blog_bloc.dart
@@ -3,15 +3,40 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../../core/usecase/usecase.dart';
 import '../../../domain/entities/blog.dart';
+import '../../../domain/usecases/get_all_blogs.dart';
+import '../../../domain/usecases/upload_blog.dart';
 
 part 'blog_event.dart';
 part 'blog_state.dart';
 
 class BlogBloc extends Bloc<BlogEvent, BlogState> {
-  BlogBloc() : super(BlogInitial()) {
-    on<BlogEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    final UploadBlog _uploadBlog;
+  final GetAllBlogs _getAllBlogs;
+  BlogBloc({required UploadBlog uploadBlog,
+  required GetAllBlogs getAllBlogs}) : _uploadBlog =uploadBlog, _getAllBlogs = getAllBlogs,super(BlogInitial()) {
+    on<BlogEvent>((event, emit) => emit(BlogLoading()));
+    on<BlogUpload>(_onBlogUpload);
+    on<BlogFetchAllBlogs>(_onFetchAllBlogs);
+  }
+
+  void _onBlogUpload(BlogUpload event, Emitter<BlogState> emit) async {
+    final res = await _uploadBlog(
+      UploadBlogParams(posterId: event.posterId, title: event.title, content: event.content, image: event.image, topics: event.topics)
+    );
+
+    res.fold(
+      (l) => emit(BlogFailure(l.message)),
+      (r) => emit(BlogUploadSuccess()),
+    );
+  }
+  void _onFetchAllBlogs(BlogFetchAllBlogs event, Emitter<BlogState> emit) async {
+    final res = await _getAllBlogs(NoParams());
+
+     res.fold(
+      (l) => emit(BlogFailure(l.message)),
+      (r) => emit(BlogsDisplaySuccess(r)),
+    );
   }
 }

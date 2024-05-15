f93f46cd2aa82499098a30e9146eb11aef50cbb8
TimurNiki <timurniki95@gmail.com>
2024-05-15 22:59:14 +0400

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'blog_app/core/common/cubits/app_user_cubit.dart';
-import 'blog_app/core/init/init_dependencies.dart';
-import 'blog_app/core/theme/theme.dart';
-import 'blog_app/features/auth/presentation/bloc/bloc/auth_bloc.dart';
-import 'blog_app/features/auth/presentation/pages/login_page.dart';
-import 'blog_app/features/blog/presentation/bloc/bloc/blog_bloc.dart';
-import 'blog_app/features/blog/presentation/pages/blog_page.dart';
 
 void main() async {
-  await initDependencies();
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => serviceLocator<AuthBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => serviceLocator<AppUserCubit>(),
-        ),
-        BlocProvider(
-          create: (_) => serviceLocator<BlogBloc>(),
-        ),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -41,18 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkThemeMode,
-      home: BlocSelector<AppUserCubit, AppUserState, bool>(
-        selector: (state) {
-          return state is AppUserLoggedIn;
-        },
-        builder: (context, isLoggedIn) {
-          if (isLoggedIn) {
-            return const BlogPage();
-          }
-          return const LoginPage();
-        },
-      ),
+      home: ,
     );
   }
 }
